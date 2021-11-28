53ce7feac657c38d58df9cbcced25002f46ed3b0
sushma1999 <56186986+sushma1999@users.noreply.github.com>
2021-11-28 21:45:53 +0530

    Update CredentialService.java

--- EmailApplication/src/com/greatlearning/service/CredentialService.java
+++ EmailApplication/src/com/greatlearning/service/CredentialService.java
@@ -3,28 +3,30 @@ import java.util.Random;
 import com.greatlearning.model.Employee;
 
 public class CredentialService {
+	
 	public char[] generatePassword() {
-		String capitalLetters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		String smallLetters="abcdefghijklmnopqrstuvwxyz";
-		String numbers="0123456789";
-		String specialCharacters="!@#$%^&*_=+-/.?<>)";
-   String values = capitalLetters+smallLetters+numbers+specialCharacters;
+		
+	 String capitalLetters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	 String smallLetters="abcdefghijklmnopqrstuvwxyz";
+	 String numbers="0123456789";
+	 String specialCharacters="!@#$%^&*_=+-/.?<>)";
+         String values = capitalLetters+smallLetters+numbers+specialCharacters;
    
-   Random random = new Random();
-   char[] password = new char[8];
-   
-   
-   for (int i=0; i<8; i++) {
-       int rand = random.nextInt(values.length());
-       password[i] = values.charAt(rand);
-	}
-   return password;
+         Random random = new Random();
+         char[] password = new char[8];
+      
+           for (int i=0; i<8; i++) {
+             int rand = random.nextInt(values.length());
+             password[i] = values.charAt(rand);
+	   }
+		
+        return password;
 	}
 	
-	public String generateEmailAddress(String firstName,String lastname ,String department)
-	{
-	return 	firstName+lastname+"@"+department+".greatlearning.com";
+	public String generateEmailAddress(String firstName,String lastname ,String department)	{
+		return 	firstName+lastname+"@"+department+".greatlearning.com";
 	}
+	
 	public void showCredentials(Employee employee, String email,char[] generatedPassword) 
 	{
 		System.out.println("Dear "+employee.getFirstName()+" your generated credentials are as follows");
