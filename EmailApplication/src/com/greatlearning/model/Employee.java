7ff78affca8b205af057085d16a13dd7e48703b7
sushma1999 <56186986+sushma1999@users.noreply.github.com>
2021-11-28 21:42:09 +0530

    Update Employee.java

--- EmailApplication/src/com/greatlearning/model/Employee.java
+++ EmailApplication/src/com/greatlearning/model/Employee.java
@@ -1,5 +1,6 @@
 package com.greatlearning.model;
 public class Employee {
+	
 	String firstName;
 	String lastName;
 	public  Employee( String firstName, String lastName) {
@@ -9,17 +10,18 @@ public class Employee {
 
 	public String getFirstName() {
 		return firstName;
-		
 	}
+
 	public void setFirstName() {
        this.firstName=firstName;
-    }
+       }
+	
 	public String getLastName() {
 		return lastName;
-	
 	}
+	
 	public void setLastName() {
 		this.lastName=lastName;
-		}
+	}
 
 }
